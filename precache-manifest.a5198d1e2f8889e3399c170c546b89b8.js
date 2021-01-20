bc9ee0eeb765c91282ff4ead0b926d2016a4c5b7
TafatsuM <turkdemirmustafa@gmail.com>
2021-01-20 14:44:33 +0300

    Updates

---
+++ precache-manifest.a5198d1e2f8889e3399c170c546b89b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54326f28230a7a646593bebcbe7774d7",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "14361e1ab1e6d76be6b8",
+    "url": "/shopping-cart/static/js/2.d1642aec.chunk.js"
+  },
+  {
+    "revision": "23900771a863482f56e68d68cf1f3072",
+    "url": "/shopping-cart/static/js/2.d1642aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab97a2867079a515f7f0",
+    "url": "/shopping-cart/static/js/main.c884b975.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  }
+]);
